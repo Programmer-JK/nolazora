1e0a4ec6ba971213e3a7c92245a0d98661bcafe9
jkpark2 <jkpark2@mcnc.co.kr>
2023-06-13 11:08:05 +0900

    0613 - navigator 추가 import - fragment간 이동 - 게임 선택 화면 xml 작성 - view Binding 추가

--- app/src/main/java/com/example/nolazora/GameSelectFragment.kt
+++ app/src/main/java/com/example/nolazora/GameSelectFragment.kt
@@ -5,11 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.navigation.fragment.findNavController
+import com.example.nolazora.databinding.FragmentGameSelectBinding
+import com.example.nolazora.databinding.FragmentTeamSettingBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -17,43 +15,15 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class GameSelectFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_select, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GameSelectFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GameSelectFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val binding = FragmentGameSelectBinding.inflate(inflater, container, false);
+        binding.btnGameEnd.setOnClickListener{
+            findNavController().navigate(R.id.action_gameSelectFragment_to_titleFragment)
+        }
+        return binding.root
     }
 }
